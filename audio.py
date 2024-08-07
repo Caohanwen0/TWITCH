0c651decd30124b1521032dd1c2cbeda2a165763
caohanwen <chw20@mails.tsinghua.edu.cn>
2024-08-07 21:04:37 +0800

    audio

--- audio.py
+++ audio.py
@@ -1,7 +1,7 @@
-
-
 import os
+from utils import get_video_number
 with open('audio.sh', 'w') as fout:
-    for i in range(0, 500):
+    video_number = get_video_number()
+    for i in range(0, video_number):
         fout.write(f'ffmpeg -i video/{i}.mkv -map 0:a -acodec libmp3lame audio/{i}.mp3\n')
 
